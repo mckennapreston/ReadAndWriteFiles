eb96296d1ee1a56f146320623ecca91f2601e068
Mckenna Preston <Mckenna_Preston1@baylor.edu>
2022-01-31 15:46:21 -0600

    in class 1/31

---
+++ client_list.py
@@ -0,0 +1,10 @@
+def main():
+    infile = open("clients.txt", "r")
+
+    counter = 1
+    for client in infile:
+        print(counter, ". ", client.rstrip("\n"), sep="")
+        counter += 1
+
+
+main()
